16b207e47fa7e4b4ced694effc746044b393cc45
dino596 <arthurliutw@gmail.com>
2024-10-24 11:36:45 -0700

    added controller and board

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/mines/MinesApiController.java
@@ -0,0 +1,10 @@
+package com.nighthawk.spring_portfolio.mvc.mines;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/casino")
+public class MinesApiController {
+
+}
---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/mines/MinesBoard.java
@@ -0,0 +1,48 @@
+package com.nighthawk.spring_portfolio.mvc.mines;
+
+import java.util.Random;
+
+public class MinesBoard {
+    private static final int BOARD_SIZE = 5;
+
+    private int[][] board = new int[BOARD_SIZE][BOARD_SIZE];
+    private int mines;
+
+    public MinesBoard(String stakes) {
+        Random rng = new Random();
+        mines = switch(stakes) {
+            case "low" -> rng.nextInt(4, 7);
+            case "medium" -> rng.nextInt(6, 9);
+            case "high" -> rng.nextInt(8, 11);
+            default -> -1;
+        };
+    }
+
+    private void placeMines() {
+        int xCoord;
+        int yCoord;
+
+        Random rng = new Random();
+
+        for (int i = 0; i < mines; i++) {
+            xCoord = rng.nextInt(0, BOARD_SIZE);
+            yCoord = rng.nextInt(0, BOARD_SIZE);
+
+        }
+    }
+
+    private void printBoard() {
+        for (int[] row : board) {
+            for (int col : row) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // unit tester
+    public static void main (String[] args) {
+        MinesBoard board = new MinesBoard("low");
+        board.printBoard();
+    }
+}
