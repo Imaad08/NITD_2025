36c84025d621ea1fb08fa542257e5dbcf2ce87b6
tanav-kambhampati <fifteentanav@gmail.com>
2024-10-31 09:09:19 -0700

    api controller

--- src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/leaderboard/LeaderboardApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/leaderboard/LeaderboardApiController.java
@@ -1,5 +1,32 @@
 package com.nighthawk.spring_portfolio.mvc.rpg.leaderboard;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/leaderboard")
 public class LeaderboardApiController {
-    
+
+    @Autowired
+    private LeaderboardJpaRepository repository;
+
+    // Endpoint to get the entire leaderboard sorted by score
+    @GetMapping
+    public List<Leaderboard> getLeaderboard() {
+        return repository.findAllByOrderByScoreDesc();
+    }
+
+    // Endpoint to add a new score entry
+    @PostMapping
+    public Leaderboard addScore(@RequestBody Leaderboard leaderboard) {
+        return repository.save(leaderboard);
+    }
+
+    // Endpoint to reset the leaderboard (for demonstration)
+    @DeleteMapping("/reset")
+    public void resetLeaderboard() {
+        repository.deleteAll();
+    }
 }
