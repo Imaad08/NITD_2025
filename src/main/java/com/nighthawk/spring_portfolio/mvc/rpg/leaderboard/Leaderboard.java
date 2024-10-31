afd30255a156aab12f53eee9aaa9c21fe4f0298b
tanav-kambhampati <fifteentanav@gmail.com>
2024-10-31 08:59:10 -0700

    leaderboard files stuff

--- src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/leaderboard/Leaderboard.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/leaderboard/Leaderboard.java
@@ -1,5 +1,37 @@
 package com.nighthawk.spring_portfolio.mvc.rpg.leaderboard;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Leaderboard {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String playerName;
+
+    @Column(nullable = false)
+    private int score;
+
+    // Optional fields for additional information, like timestamp
+    @Column(nullable = false)
+    private String date;
+
+    public Leaderboard(String playerName, int score, String date) {
+        this.playerName = playerName;
+        this.score = score;
+        this.date = date;
+    }
 }
