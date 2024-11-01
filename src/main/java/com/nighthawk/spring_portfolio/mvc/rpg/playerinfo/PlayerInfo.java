7ee5aa32c3d29a7431f3c8130aa754e712058293
SriS126 <smsurapaneni@gmail.com>
2024-11-01 08:48:02 -0700

    backend fix

--- src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/playerinfo/PlayerInfo.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/playerinfo/PlayerInfo.java
@@ -1,7 +1,5 @@
 package com.nighthawk.spring_portfolio.mvc.rpg.playerinfo;
 
-import java.util.ArrayList;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,10 +8,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-
-
 public class PlayerInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,23 +27,3 @@ public class PlayerInfo {
         this.period = period;
     }
 }
-
-public static PlayerInfo[] init() {
-        ArrayList<PlayerInfo> players = new ArrayList<>();
-        players.add(new PlayerInfo("Tanav", 1, 1500));
-        players.add(new PlayerInfo("Bob", 2, 1200));
-        players.add(new PlayerInfo("Charlie", 3, 1000));
-        return players.toArray(new PlayerInfo[0]);  // Converts List to array
-    }
-
-    // Main 
-    public static void main(String[] args) {
-        // Initialize leaderboard entries
-        PlayerInfo[] players = init();
-
-        // Enhanced for loop to print each leaderboard entry
-        for (PlayerInfo leader : players) {
-            System.out.println(players);  // Print leaderboard object
-        }
-    }
-
