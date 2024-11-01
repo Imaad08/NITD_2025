aeaec821352f2b7f184a8ca473b4da1b64a4cded
tanayshah1 <dhasushah@gmail.com>
2024-11-01 09:28:24 -0700

    fixed again

--- src/main/java/com/nighthawk/spring_portfolio/mvc/stocks/User.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/stocks/User.java
@@ -24,6 +24,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -36,7 +37,7 @@ import lombok.NoArgsConstructor;
 @Entity
 class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.UUID)
     private UUID id;
 
     @Column(unique = true, nullable = false)
