7cc9a3ef7f38194b30cf06780cf11876b8b9461e
tanav-kambhampati <fifteentanav@gmail.com>
2024-11-05 09:07:48 -0800

    new query

--- src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/answer/AnswerJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/answer/AnswerJpaRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.nighthawk.spring_portfolio.mvc.rpg.player.Player;
 import com.nighthawk.spring_portfolio.mvc.rpg.question.Question;
@@ -11,4 +12,7 @@ import com.nighthawk.spring_portfolio.mvc.rpg.question.Question;
 public interface AnswerJpaRepository extends JpaRepository<Answer, Long> {
     List<Answer> findByQuestionId(Long question);
     Optional<Answer> findByQuestionAndPlayer(Question question, Player player);
+
+    @Query("SELECT p.name, SUM(a.chatScore) AS totalScore FROM Answer a JOIN a.player p GROUP BY p.name ORDER BY totalScore DESC LIMIT 10")
+    List<Object[]> findTop10PlayersByTotalScore();
 }
