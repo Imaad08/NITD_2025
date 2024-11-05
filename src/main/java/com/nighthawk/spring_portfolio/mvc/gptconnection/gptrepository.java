9fc35f6f719c319d43a3125b3cfcb58967d31840
tanav-kambhampati <fifteentanav@gmail.com>
2024-11-04 18:10:13 -0800

    remove comments

--- src/main/java/com/nighthawk/spring_portfolio/mvc/gptconnection/gptrepository.java
+++
@@ -1,7 +0,0 @@
-// package com.nighthawk.spring_portfolio.mvc.gptconnection;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-
-// @Repository
-// public interface GptRepository extends JpaRepository<GptEntry, Long> {
-// }
