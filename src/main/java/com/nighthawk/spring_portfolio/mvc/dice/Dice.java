76024b5a79a6f9463d97661fd332ad88bdefe7c8
hanlunli <hanlunli.007@gmail.com>
2024-12-03 09:40:47 -0800

    fixing dice calculation

--- src/main/java/com/nighthawk/spring_portfolio/mvc/dice/Dice.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/dice/Dice.java
@@ -18,7 +18,7 @@ public class Dice {
         Random rand = new Random();
         double rand_double = rand.nextDouble(1);
         if (rand_double <= winChance) {
-            return this.betSize*(1/this.winChance - 1) * this.winChance * (1- 0.05/(1-this.winChance));
+            return this.betSize*(1.95 - this.winChance)/this.winChance;
         } else { 
             return 0-this.betSize;
         }
