c3f15251221b988497d1e33017266d6fd4d8cd74
tanav-kambhampati <fifteentanav@gmail.com>
2024-12-06 08:46:20 -0800

    fixed query

--- src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/answer/AnswerJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/rpg/answer/AnswerJpaRepository.java
@@ -3,13 +3,13 @@ package com.nighthawk.spring_portfolio.mvc.rpg.answer;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AnswerJpaRepository extends JpaRepository<Answer, Long> {
     List<Answer> findByQuestionId(Long question);
     List<Answer> findByUserId(Integer userid);
 
-    /*
-    @Query("SELECT NEW com.nighthawk.spring_portfolio.mvc.rpg.answer.LeaderboardDto(p.id, SUM(a.chatScore)) FROM Answer a JOIN a.player p GROUP BY p.id ORDER BY SUM(a.chatScore) DESC")
+    @Query("SELECT NEW com.nighthawk.spring_portfolio.mvc.rpg.answer.LeaderboardDto(p.user_id, SUM(a.chatScore)) FROM Answer a JOIN a.player p GROUP BY p.user_id ORDER BY SUM(a.chatScore) DESC")
     List<LeaderboardDto> findTop10PlayersByTotalScore();
-    */
+    
 }
